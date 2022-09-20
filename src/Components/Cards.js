1ac31046373521eedeb1a16544c22217d3eb5fb2
umarsuhail112 <umarsuhail112@gmail.com>
2022-09-20 19:29:56 +0530

    responsiveness

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -3,10 +3,11 @@ import "./Cards.css";
 
 export default function Cards({ title, technologies, cardNo, cardIcon }) {
   return (
-    <div className={`card card-${cardNo} z-10 md:w-52 w-full`}>
+    <div className={`card card-${cardNo} z-10 w-96`}>
       <div className="card__icon">{cardIcon}</div>
       <h2 className="card__title text-black font-bold text-xl">{title}</h2>
-      <div className="overflow-auto">
+      <div className="overflow-auto flex m-2">
+        {technologies}
       </div>
       <p className="card__apply text-gray text-sm">
         <a className="card__link text-sm" href="https://wa.me/9497656243">
--- src/Layouts/Landing.js
+++ src/Layouts/Landing.js
@@ -1,25 +1,24 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  solid,
-  brands,
-} from "@fortawesome/fontawesome-svg-core/import.macro";
+import { solid, brands } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 import "./Landing.css";
 import Cards from "../Components/Cards";
 
 export default function Landing(props) {
-
-const theme = props.theme
+  const theme = props.theme;
   return (
     <div className={`h-auto w-full" ${theme ? "bg-blaack" : "bg-white"}`}>
-
       <div className=" flex flex-col ">
         <div className="m-3  p-6">
           <span className="font-rale font-black md:text-5xl text-2xl text-kap text-left">
             Collaborate
           </span>
-          <span className={`font-rale font-black md:text-5xl text-2xl  text-left" ${theme ? 'text-white' :'text-black'}`}>
+          <span
+            className={`font-rale font-black md:text-5xl text-2xl  text-left" ${
+              theme ? "text-white" : "text-black"
+            }`}
+          >
             {" "}
             for your digital experience
           </span>
@@ -28,7 +27,7 @@ const theme = props.theme
         <div className=" flex md:wrap md:justify-around overflow-auto">
           <Cards
             title="Web Application development"
-            technologies="Angular | React js | Veu JS | Node js | Python | Wordpress |"
+            technologies="Web applications, E-Commerce, CMS, B2B, ERP/CRM"
             cardNo={1}
             cardIcon={
               <FontAwesomeIcon icon={brands("angular")} bounce color="white" />
@@ -36,7 +35,7 @@ const theme = props.theme
           />
           <Cards
             title="Mobile-App development"
-            technologies="Angular | React js | Veu JS | Node js | Python | Wordpress |"
+            technologies="Android & IOS Applications, E-Commerce, Hybrid & native Applications"
             cardNo={2}
             cardIcon={
               <FontAwesomeIcon
@@ -47,8 +46,8 @@ const theme = props.theme
             }
           />
           <Cards
-            title="Digital Marketing / SEO"
-            technologies="Angular | React js | Veu JS | Node js | Python | Wordpress |"
+            title="Digital Marketing"
+            technologies="Web Analytics, Content Development, SEO, Email Marketing & Social Media Marketing"
             cardNo={3}
             cardIcon={
               <FontAwesomeIcon
@@ -59,8 +58,8 @@ const theme = props.theme
             }
           />
           <Cards
-            title="Business process outsourcing"
-            technologies="Angular | React js | Veu JS | Node js | Python | Wordpress |"
+            title="Business process outsourcing & Consulting"
+            technologies="IT Consulting, Market Research, Tele-Services, Marketing Solutions"
             cardNo={4}
             cardIcon={
               <FontAwesomeIcon icon={solid("headset")} bounce color="white" />
@@ -71,9 +70,14 @@ const theme = props.theme
     </div>
   );
 }
-<a href="#_" class="relative inline-flex items-center justify-center px-6 py-3 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group">
-<span class="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-purple-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
-<span class="absolute inset-0 w-full h-full bg-white rounded-md "></span>
-<span class="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-purple-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
-<span class="relative text-purple-600 transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">Button Text</span>
-</a>
+<a
+  href="#_"
+  class="relative inline-flex items-center justify-center px-6 py-3 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group"
+>
+  <span class="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-purple-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+  <span class="absolute inset-0 w-full h-full bg-white rounded-md "></span>
+  <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-purple-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
+  <span class="relative text-purple-600 transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">
+    Button Text
+  </span>
+</a>;
